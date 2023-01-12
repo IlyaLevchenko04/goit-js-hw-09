989f983ffd5e0355d5ab402c9b657735ab14bbaf
IlyaLevchenko04 <109174985+IlyaLevchenko04@users.noreply.github.com>
2023-01-12 17:48:39 +0000

    Deploying to gh-pages from @ IlyaLevchenko04/goit-js-hw-09@0aa02a1a5111fa66bbf1506127b88120974f657b 🚀

--- 03-promises.1cc5ecc1.js
+++
@@ -1,2 +0,0 @@
-const e=document.getElementsByName("delay"),o=document.getElementsByName("step"),t=document.getElementsByName("amount");console.log(e),console.log(o),console.log(t),console.log("ebat");
-//# sourceMappingURL=03-promises.1cc5ecc1.js.map
--- 03-promises.dd14196a.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.getElementsByName("delay"),o=document.getElementsByName("step"),n=document.getElementsByName("amount");console.log(e),console.log(o),console.log(n),console.log("ebat")}();
-//# sourceMappingURL=03-promises.dd14196a.js.map
