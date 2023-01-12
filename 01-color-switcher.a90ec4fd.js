1e4b76611e393b9cc377d78b04bc9b1cdd97071b
IlyaLevchenko04 <109174985+IlyaLevchenko04@users.noreply.github.com>
2023-01-12 16:22:48 +0000

    Deploying to gh-pages from @ IlyaLevchenko04/goit-js-hw-09@f6e5b8a0b2e78b9fa02ebdb204af7fc8d8342dff 🚀

---
+++ 01-color-switcher.a90ec4fd.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");let r=null;const o=document.querySelector("body");e.setAttribute("disabled","true"),t.addEventListener("click",(function(){t.setAttribute("disabled","true"),r=setInterval((()=>{o.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),e.removeAttribute("disabled")})),e.addEventListener("click",(function(){clearInterval(r),t.removeAttribute("disabled"),e.setAttribute("disabled","true")}));
+//# sourceMappingURL=01-color-switcher.a90ec4fd.js.map
---
+++ 03-promises.e35212ad.js
@@ -0,0 +1,2 @@
+!function(){var e=document.getElementsByName("delay"),o=document.getElementsByName("step"),l=document.getElementsByName("amount");console.log(e),console.log(o),console.log(l),console.log("hello")}();
+//# sourceMappingURL=03-promises.e35212ad.js.map
